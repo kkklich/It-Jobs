f3d30ada8db14a076cf4bb053b57118c12028e2b
krzysztof.klich <krzysztof.klich@idealogic.pl>
2026-07-07 20:21:35 +0200

    update IP

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 DB_CONFIG = {
-    'host': 'krzysztofklich.pl',
+    'host': '93.157.102.234',
     'user': 'nlddzucmzp_krzysztof',
     'password': 'RmoOle4T52csHng!@#',
     'database': 'nlddzucmzp_Jobs_IT'
